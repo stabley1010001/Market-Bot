e4d69d6a519556cca78c9a5736ffda80db99016d
Stanley Tang <stang0926@gmail.com>
2019-12-12 20:58:20 -0500

    another one

--- Market Bot.py
+++ Market Bot.py
@@ -2,9 +2,10 @@
 import os
 import discord
 import sqlite3
-import database
+from database import MarketDatabase
 from discord.ext import commands
 
+db = MarketDatabase()
 TOKEN = os.environ['DISCORD_TOKEN']
 GUILD = os.environ['DISCORD_GUILD']
 
@@ -25,11 +26,11 @@ async def on_ready():
     print(f'Guild Members:\n - {members}')
     categories = '\n - '.join([cat.name for cat in guild.categories])
     print(f'Categories:\n - {categories}')
-    check_users()
+    db.check_users()
 
 @bot.event
 async def on_member_join(member):
-    await update_user(member.name, 1, 0)
+    await db.update_user(member.name, 1, 0)
     await member.create_dm()
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to the Free Market Discord server!'
@@ -37,20 +38,20 @@ async def on_member_join(member):
 
 @bot.command(name='create_new_shop', help='Creates a new private text channel')
 async def create_new_shop(ctx, name):
-    user_data = get_user(ctx.message.author.name)
+    user_data = db.get_user(ctx.message.author.name)
     if user_data["rank"] == user_data["num_shops"]:
         ctx.send("You have reached the maximum number of shops you can own")
         return
     user_data["num_shops"] = user_data["num_shops"] + 1
     cat = discord.utils.get(ctx.guild.categories, name="Front Page")
-    await update_user(user_data)
+    await db.update_user(user_data)
     await ctx.message.guild.create_text_channel(name, category=cat)
     await ctx.send(f'New text channel {name} created!')
     await ctx.send(f"You now have {user_data['num_shops']}/{user_data['rank']} shops")
 
 @bot.command(name='info', help='Lists your information')
 async def list_info(ctx):
-    user_data = get_user(ctx.message.author.name)
+    user_data = db.get_user(ctx.message.author.name)
     await ctx.send(f"Rank: {user_data['rank']}")
     await ctx.send(f"Shops: {user_data['num_shops']}/{user_data['rank']}")
 bot.run(TOKEN)
--- database.py
+++ database.py
@@ -1,40 +1,42 @@
 import sqlite3
 import os
+class MarketDatabase:
+    
+    def __init__(self):
+        self.con = sqlite3.connect('database.sqlite3')
+        self.cur = self.con.cursor()
+    
+    def check_tables(self):
+        cur = self.cur
+        check_tables_sql = "SELECT name FROM sqlite_master WHERE type = 'table'"
+        cur.execute(check_tables_sql)
+        print(cur.fetchall())
 
-con = sqlite3.connect('database.sqlite3')
-cur = con.cursor()
+    def check_users(self):
+        cur = self.cur
+        check_users_sql = "SELECT id, name, rank, num_shops FROM users"
+        cur.execute(check_users_sql)
+        formatted_result = [f"{id:<5}{name:<30}{rank:<20}{num_shops:>11}" for id, name, rank, num_shops in cur.fetchall()]
+        id, name, rank, num_shops = "Id", "User", "Rank", "Shops owned"
+        print('\n'.join([f"{id:<5}{name:<30}{rank:<20}{num_shops:>11}"] + [""] + formatted_result))
 
-def check_tables():
-    global cur
-    check_tables_sql = "SELECT name FROM sqlite_master WHERE type = 'table'"
-    cur.execute(check_tables_sql)
-    print(cur.fetchall())
+    def update_user(self, name, rank, num_shops):
+        cur = self.cur
+        update_user_sql = "INSERT INTO users (name, rank, num_shops) VALUES (?, ?, ?)"
+        cur.execute(update_user_sql, (name, rank, num_shops))
+        self.con.commit()
 
-def check_users():
-    global cur
-    check_users_sql = "SELECT id, name, rank, num_shops FROM users"
-    cur.execute(check_users_sql)
-    formatted_result = [f"{id:<5}{name:<30}{rank:<20}{num_shops:>11}" for id, name, rank, num_shops in cur.fetchall()]
-    id, name, rank, num_shops = "Id", "User", "Rank", "Shops owned"
-    print('\n'.join([f"{id:<5}{name:<30}{rank:<20}{num_shops:>11}"] + [""] + formatted_result))
+    def update_user(self, user_data):
+        cur = self.cur
+        update_user(user_data["name"], user_data["rank"], user_data["num_shops"])
 
-def update_user(name, rank, num_shops):
-    global cur
-    update_user_sql = "INSERT INTO users (name, rank, num_shops) VALUES (?, ?, ?)"
-    cur.execute(update_user_sql, (name, rank, num_shops))
-    con.commit()
-
-def update_user(user_data):
-    global cur
-    update_user(user_data["name"], user_data["rank"], user_data["num_shops"])
-
-def get_user(name):
-    global cur
-    get_user_sql = "SELECT rank, num_shops FROM users WHERE name = ?"
-    cur.execute(get_user_sql, (name,))
-    result = cur.fetchall()[0]
-    output = {"name" : name, "rank" : result[0] , "num_shops" : result[1]}
-    return output
+    def get_user(self, name):
+        cur = self.cur
+        get_user_sql = "SELECT rank, num_shops FROM users WHERE name = ?"
+        cur.execute(get_user_sql, (name,))
+        result = cur.fetchall()[0]
+        output = {"name" : name, "rank" : result[0] , "num_shops" : result[1]}
+        return output
 '''
 cur.execute("DROP TABLE users")
 setup_users_table = "CREATE TABLE users (id integer PRIMARY KEY, name text NOT NULL, rank integer, num_shops integer, UNIQUE(id, name))"
