e5e6e77d5badfbf1032c596f1859d748e5fb0e46
Stanley Tang <stang0926@gmail.com>
2019-12-13 19:58:11 -0500

    another one

--- Market Bot.py
+++ Market Bot.py
@@ -43,13 +43,21 @@ async def create_new_shop(ctx, name):
     if user_data["rank"] == user_data["num_shops"]:
         await ctx.send("You have reached the maximum number of shops you can own")
     else:
-        user_data = db.get_user(ctx.message.author.name)
-        user_data["num_shops"] += 1
-        db.update_user_by_data(user_data)
-        cat = discord.utils.get(ctx.guild.categories, name="Front Page")
-        await ctx.message.guild.create_text_channel(name, category=cat)
-        await ctx.send(f'New text channel {name} created!')
-        await ctx.send(f"You now have {user_data['num_shops']}/{user_data['rank']} shops")
+        if(db.add_shop(name, user_data["name"]) == "success"):
+            cat = discord.utils.get(ctx.guild.categories, name="Front Page")
+            await ctx.message.guild.create_text_channel(name, category=cat)
+            await ctx.send(f'New text channel {name} created!')
+            await ctx.send(f"You now have {user_data['num_shops']}/{user_data['rank']} shops")
+        else:
+            await ctx.send("Shop name taken...")
+
+@bot.command(name='remove_shop', help='Remove one of your shops')
+async def remove_shop(ctx, shop_name):
+    owner = ctx.message.author.name
+    if(db.remove_shop(owner, shop_name) == "success"):
+        await ctx.send(f"{shop_name} has been removed. You now have {free_spots} spots for shops.")
+    else:
+        await ctx.send("Invalid shop name...")
 
 @bot.command(name='info', help='Lists your information')
 async def list_info(ctx):
--- database.py
+++ database.py
@@ -12,6 +12,14 @@ class MarketDatabase:
         cur.execute(check_tables_sql)
         print(cur.fetchall())
 
+    def check_shops(self):
+        cur = self.cur
+        check_shops_sql = "SELECT id, name, owner FROM shops"
+        cur.execute(check_shops_sql)
+        formatted_result = [f"{id:<5}{name:<30}{owner:<30}" for id, name, owner in cur.fetchall()]
+        id, name, owner = "Id", "Name", "Owner"
+        print('\n'.join([f"{id:<5}{name:<30}{owner:<30}"] + [""] + formatted_result))
+    
     def check_users(self):
         cur = self.cur
         check_users_sql = "SELECT id, name, rank, num_shops FROM users"
@@ -35,6 +43,39 @@ class MarketDatabase:
         cur.execute(update_user_sql, (name, name, rank, num_shops))
         self.con.commit()
 
+    def add_shop(self, name, owner):
+        cur = self.cur
+        try:
+            add_shop_sql = "INSERT INTO shops (name, owner) VALUES (?, ?)"
+            cur.execute(add_shop_sql, (name, owner))
+            u = self.get_user(owner)
+            u["num_shops"] += 1
+            self.update_user_by_data(u)
+            self.con.commit()
+            return "success"
+        except:
+            return "failure"
+
+    def remove_shop(self, name, owner):
+        cur = self.cur
+        remove_shop_sql = "DELETE FROM shops WHERE name = ? AND owner = ?"
+        try:
+            cur.execute(remove_shop_sql, (name, owner))
+            u = self.get_user(owner)
+            u["num_shops"] -= 1
+            self.update_user_by_data(u)
+            self.con.commit()
+            return "success"
+        except:
+            return "failure"
+
+    def get_shops_owned(self, owner):
+        cur = self.cur
+        get_shops_sql = "SELECT name FROM shops WHERE owner = ?"
+        cur.execute(get_shops_sql, (owner,))
+        shops = [x[0] for x in cur.fetchall()]
+        return shops
+    
     def get_user(self, name):
         cur = self.cur
         get_user_sql = "SELECT rank, num_shops FROM users WHERE name = ?"
@@ -42,18 +83,25 @@ class MarketDatabase:
         result = cur.fetchall()[0]
         output = {"name" : name, "rank" : result[0] , "num_shops" : result[1]}
         return output
+
 db = MarketDatabase()
 '''
+db.cur.execute("DROP TABLE shops")
+setup_shops_table = "CREATE TABLE shops (id integer PRIMARY KEY, name text NOT NULL UNIQUE, owner text NOT NULL)"
+db.cur.execute(setup_shops_table)
+db.con.commit()
+db.check_tables()
+
 db.cur.execute("DROP TABLE users")
 setup_users_table = "CREATE TABLE users (id integer PRIMARY KEY, name text NOT NULL, rank integer, num_shops integer)"
 db.cur.execute(setup_users_table)
-'''
+
 db.update_user("Stan", 1, 0)
 db.update_user("GapingThrowrer20", 1, 0)
 db.update_user("atmost", 1, 0)
 db.update_user("Nav", 1, 0)
 db.check_users()
-'''
+
 u = db.get_user("Stan")
 u["num_shops"] += 1
 db.update_user_by_data(u)
@@ -61,6 +109,3 @@ db.check_users()
 
 db.con.commit()
 '''
-
-
-
